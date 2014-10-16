622f7271a2f4d15d01be20268a8adda87588b03b
roeku <donmezfurkan@gmail.com>
2014-10-16 08:22:37 +0200

    initial commit

---
+++ Assets/Qualcomm Augmented Reality/Scripts/ImageTargetPlayAudio.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageTargetPlayAudio : MonoBehaviour,
+ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	
+	void Start()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Play audio when target is found
+			audio.Play();
+		}
+		else
+		{
+			// Stop audio when target is lost
+			audio.Stop();
+		}
+	}   
+}
